d73c38f10aaa1c9827eda5a45bb124f2c36ac6d6
Dawson Field <dawsonf@ksu.edu>
2020-04-20 15:50:14 -0500

    Mostly done (null exception)

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -9,10 +9,53 @@ namespace Movies.Pages
 {
     public class IndexModel : PageModel
     {
+        /// <summary>
+        /// The movies to display on the index page 
+        /// </summary>
+        public IEnumerable<Movie> Movies { get; protected set; }
 
-        public void OnGet()
+        /// <summary>
+        /// The current search terms 
+        /// </summary>
+        public string SearchTerms { get; set; }
+
+        /// <summary>
+        /// The filtered MPAA Ratings
+        /// </summary>
+        public string[] MPAARatings { get; set; }
+
+        /// <summary>
+        /// The filtered genres
+        /// </summary>
+        [BindProperty]
+        public string[] Genres { get; set; }
+
+        /// <summary>
+        /// The minimum IMDB Rating
+        /// </summary>
+        [BindProperty]
+        public double? IMDBMin { get; set; }
+
+        /// <summary>
+        /// The maximum IMDB Rating
+        /// </summary>
+        [BindProperty]
+        public double? IMDBMax { get; set; }
+
+        /// <summary>
+        /// Gets the search results for display on the page
+        /// </summary>
+        public void OnGet(string SearchTerms, string[] MPAARatings, string[] Genre, double? IMDBMin, double? IMDBMax)
         {
-
+            SearchTerms = Request.Query["SearchTerms"];
+            MPAARatings = Request.Query["MPAARatings"];
+            Genres = Request.Query["Genres"];
+            this.IMDBMin = IMDBMin;
+            this.IMDBMax = IMDBMax;
+            Movies = MovieDatabase.Search(SearchTerms);
+            Movies = MovieDatabase.FilterByMPAARating(Movies, MPAARatings);
+            Movies = MovieDatabase.FilterByGenre(Movies, Genres);
+            Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
         }
     }
 }
